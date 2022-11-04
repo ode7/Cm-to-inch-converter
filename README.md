6577dddf4baa8d3687d279220927719427c43c0f
ode7 <112641167+ode7@users.noreply.github.com>
2022-11-05 04:42:20 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cm-to-inch-converter
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-qswgyn)
---
+++ script.js
@@ -0,0 +1,10 @@
+let inputEl = document.querySelector('#inputvalue');
+let outputEl = document.querySelector('#outputvalue');
+let ans;
+parseInt(ans);
+function cmToinch() {
+  parseInt(ans);
+  ans = inputEl.value / 2.54;
+
+  outputEl.textContent = ans;
+}
